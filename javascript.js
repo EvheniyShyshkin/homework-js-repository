19e3d272e3155de8de0ed18ad1c3c0167a228a25
Evhenich <evhenich@Evhenichs-MacBook-Air.local>
2019-11-29 15:03:53 +0200

    added table with restcountries

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
-$('table').click(function(e){
-    console.log(e.target.innerText);
+$.ajax({
+    url: 'https://restcountries.eu/rest/v2/all',
+    success: function(data) {
+        console.log(data);
+        var str = '';
+
+for(let el of data) {
+        str += '<tr><td>' + el.name + '<td><td>' + el.region + '<td><td>' + el.population
+        + '<td><td>' + el.area + '<td><td>' + el.flag + '<td><td>' + el.numericCode + '</td></tr>';
+        $('table').html(str);
+    }
+ }  
 });
+
+
