64ff4225bb37db3fa22ecbcfdb4ba681c0be227b
Evhenich <evhenich@Evhenichs-MacBook-Air.local>
2019-11-29 15:09:55 +0200

    table click function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,4 @@
-$.ajax({
-    url: 'https://restcountries.eu/rest/v2/all',
-    success: function(data) {
-        console.log(data);
-        var str = '';
-
-for(let el of data) {
-        str += '<tr><td>' + el.name + '<td><td>' + el.region + '<td><td>' + el.population
-        + '<td><td>' + el.area + '<td><td>' + el.flag + '<td><td>' + el.numericCode + '</td></tr>';
-        $('table').html(str);
-    }
- }  
-});
-
+$('table').click(function(e){
+        console.log(e.target.innerText);
+}); 
 
